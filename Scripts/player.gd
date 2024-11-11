22b54711de13f5e3b707ff415723ab9da3ea3ad7
UnChiller <81498434+UnChiller@users.noreply.github.com>
2024-11-10 19:10:41 -0600

    add wall slide and grab

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -20,8 +20,9 @@ var coyote_time = 0
 # =======================
 
 func _physics_process(delta):
+	apply_gravity(delta)
 	move_check()
-	jump_check(delta)
+	jump_check()
 
 	anim_state_check()
 
@@ -31,25 +32,51 @@ func _physics_process(delta):
 # Movement Handling
 # =======================
 
-func wall_check():
+func wall_grip_left() -> float:
+	var scaled_trigger_strength = clamp((Input.get_action_raw_strength("grab_left")-.1)/.7,0,1)
+	return (2-scaled_trigger_strength)*scaled_trigger_strength
+func wall_grip_right() -> float:
+	var scaled_trigger_strength = clamp((Input.get_action_raw_strength("grab_right")-.1)/.7,0,1)
+	return (2-scaled_trigger_strength)*scaled_trigger_strength
+
+func apply_gravity(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		coyote_time -= 1
+		velocity += get_gravity() * delta
+		var wall_side = wall_check(2)
+		var wgl=wall_grip_left()
+		var wgr=wall_grip_right()
+		if (wgl or wgr) and wall_side:
+			anim_state = "grab"
+		if velocity.y >= 0:
+			# sqrt*4 is the same as 16th root, pow(
+			if wall_side == 1:
+				velocity.y *= pow(1 - wgl,1./16.)
+			elif wall_side == -1:
+				velocity.y *= pow(1 - wgr,1./16.)
+	else:
+		coyote_time = 6
+
+func wall_check(dist):
 	var space_state = get_world_2d().direct_space_state
 	for y in [0,12,24,36,48,60,72,84,96,108,120]:
 		var query = PhysicsRayQueryParameters2D.create(position+Vector2(0,-y),
-			position+Vector2(40 + WALL_JUMP_COLL_DIST,-y))
+			position+Vector2(40 + dist,-y))
 		query.exclude = [self]
 		var result = space_state.intersect_ray(query)
 		if result != {}:
 			return -1
 	for y in [0,12,24,36,48,60,72,84,96,108,120]:
 		var query = PhysicsRayQueryParameters2D.create(position+Vector2(0,-y),
-			position+Vector2(-40 - WALL_JUMP_COLL_DIST,-y))
+			position+Vector2(-40 - dist,-y))
 		query.exclude = [self]
 		var result = space_state.intersect_ray(query)
 		if result != {}:
 			return 1
 	return 0
 
-func jump_check(delta):
+func jump_check():
 	# Landing
 	if anim_state == "jump" and is_on_floor():
 		anim_state = "idle"
@@ -60,27 +87,22 @@ func jump_check(delta):
 		jump_grace = 6 # 6t/60tps = 100ms 
 	elif jump_grace > 0:
 		jump_grace -= 1
-	
-	# Add the gravity.
-	if not is_on_floor():
-		coyote_time -= 1
-		velocity += get_gravity() * delta
-	else:
-		coyote_time = 6
+
 	# Handle jump.
-	var wall_side = wall_check()
+	var wall_side = wall_check(WALL_JUMP_COLL_DIST)
 	if wall_side and jump_grace > 0 and not is_on_floor():
 		jump_grace = 0
 		coyote_time = 0
 		velocity.x += wall_side * 1500
 		velocity.y = JUMP_VELOCITY
+		anim_state = "jump"
+		is_jumping = true
 	if coyote_time > 0 and jump_grace > 0:
 		jump_grace = 0
 		coyote_time = 0
 		velocity.y = JUMP_VELOCITY
 		anim_state = "jump"
 		is_jumping = true
-
 
 func move_check():
 	# Get the input direction and handle the movement/deceleration.
@@ -94,10 +116,10 @@ func move_check():
 		if velocity.x > 0: anim_sprite.flip_h = false
 		else: if velocity.x < 0: anim_sprite.flip_h = true
 		
-		if (anim_state != "jump"): anim_state = "walk"
+		if (anim_state != "jump" and anim_state != "grab"): anim_state = "walk"
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if (anim_state != "jump"): anim_state = "idle"
+		if (anim_state != "jump" and anim_state != "grab"): anim_state = "idle"
 
 	move_and_slide()
 
